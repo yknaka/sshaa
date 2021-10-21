a5b76f68275a7b71f9591f63254a860c4b04ddc0
yknaka <86539208+yknaka@users.noreply.github.com>
2021-10-21 22:29:16 +0900

    Added whois history

--- yknsshaalysis.py
+++ yknsshaalysis.py
@@ -5,12 +5,24 @@ import json
 import time
 import circlify
 import matplotlib.pyplot as plt
+import pickle
+dic_ip_hisotry=None
+def _trim(string):
+    if len(string) == 0:
+        return ''
+    elif string[0] == '\'' or string[0] == '\"':
+        return string[1:-1]
+    else :
+        return string
+
 
 def main(args):
     local_addr="/var/log/auth.log"
     show_top=5
     ignore_trial_less_than=50
     whois_url="http://ipwhois.app/json/xxx"
+    ip_dict="ip_whois_history.pkl"
+    export_filename="sshanalysis_result.png"
     if type(args) is str:
         args=[args]
     for arg in args:
@@ -23,14 +35,31 @@ def main(args):
             ignore_trial_less_than=int(_trim(arg[12:]))
         elif arg_.startswith('whois_url='):
             whois_url=int(_trim(arg[10:]))
+        elif arg_.startswith('ip_dict='):
+            ip_dict=_trim(arg[8:])
+            if ip_dict == "None":
+                ip_dict=None
+    print("reading auth.log file...")
     df=pd.read_table(local_addr, header=None)
+    print("analyzing log")
     df2=df[df[0].str.contains("Failed|Invalid user")]
     df_ipfreq=analyzeAuth_fast(df2)
     # 絞り込み
     df_ipfreq_=df_ipfreq[df_ipfreq["count"]>ignore_trial_less_than]
-    df_ctfreq_=do_whois(whois_url,df_ipfreq_)
+    if ip_dict is None:
+        dic_ip_history=None
+    else:
+        print("loading whois history")
+        dic_ip_history=loadLibrary(ip_dict)
+    print("calling whois")
+    df_ctfreq_,dic_ip_history=do_whois(whois_url,df_ipfreq_,dic_ip_history)
+    if dic_ip_history is not None:
+        print("exporting whois history")
+        saveLibrary(ip_dict,dic_ip_history)
+    print("grouping countries")
     dfs_list=country_grouping(df_ctfreq_)
-    show_graph(show_top,dfs_list)
+    show_graph(show_top,dfs_list,export_filename)
+
 
 def analyzeAuth_fast(df):
     ip_list=[]
@@ -92,19 +121,51 @@ def analyzeAuth(df):
     sr_ipcnt=pd.DataFrame(ip_arr,columns=['ip','user','port','pwd_atk']).ip.value_counts()
     return pd.DataFrame({'count':sr_ipcnt})
 
-def do_whois(whois_url,df):
-    headers = {"content-type": "application/json"}
+
+def saveLibrary(libaddr,my_dict):
+    try:
+        with open(libaddr, "wb") as tf:
+            pickle.dump(my_dict,tf)
+    except Exception as e:
+        print(e)
+def loadLibrary(libaddr):
+    try:
+        with open(libaddr, "rb") as tf:
+            new_dict = pickle.load(tf)
+        return new_dict
+    except Exception as e:
+        print(e)
+        return {}
+
+
+headers = {"content-type": "application/json"}
+def whoisCountry(whois_url,ip_address,defaultValue):
+    url=whois_url.replace('xxx',ip_address)
+    response=requests.get(url, headers=headers)
+    data = response.json()
+    if "country" in data:
+        return data["country"]
+    else:
+        return defaultValue
+def do_whois(whois_url,df,dic_ip_history):
     dic_country={}
+    is_ip_history_enabled=dic_ip_history is not None
     #st=time.time()
-    for i, v in df.iterrows():
-        url=whois_url.replace('xxx',i)
-        response=requests.get(url, headers=headers)
-        data = response.json()
-        dic_country[i]=data["country"]
-    #print("finish time:"+str(time.time()-st))
+    if is_ip_history_enabled:
+        for ip_address, v in df.iterrows():
+            if ip_address in dic_ip_history:
+                country=dic_ip_history[ip_address]["name"]
+            else:
+                country=whoisCountry(whois_url,ip_address,"N/A")
+                dic_ip_history[ip_address]={"name":country,"register":time.time()}
+            dic_country[ip_address]=country
+    else:
+        for ip_address, v in df_ip3.iterrows():
+            dic_country[ip_address]=whoisCountry(whois_url,"N/A")
     df_country=pd.DataFrame({"country":dic_country})
     df_country['count']=df['count']
-    return df_country
+    return df_country, dic_ip_history
+
 
 def country_grouping(df):
     dfs=df.groupby("country").sum()
@@ -115,7 +176,8 @@ def country_grouping(df):
     dfs_list=sorted(dfs_list, key=lambda x: -x['attack'])
     return dfs_list
 
-def show_graph(show_top,dfs_list):
+
+def show_graph(show_top,dfs_list,export_filename):
     count=0
     top_country=[]
     top_attack=[]
@@ -157,14 +219,8 @@ def show_graph(show_top,dfs_list):
               va='center',
               ha='center'
          )
-    plt.savefig("sshanalysis_result.png")
+    plt.savefig(export_filename)
     plt.show()
 
-def _trim(string):
-    if len(string) == 0:
-        return ''
-    elif string[0] == '\'' or string[0] == '\"':
-        return string[1:-1]
-    else :
-        return string
+
 main(sys.argv)
