159d4562b915a53f9146fa15e19a5e809acb8cdd
Yuki NAKAMURA <86539208+yknaka@users.noreply.github.com>
2021-10-24 20:22:28 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="yknsshanalysis",
-    version="1.1.",
+    version="1.1.1",
     author="Yuki NAKAMURA",
     author_email="naka_yk@live.jp",
     description="Analyze auth.log",
